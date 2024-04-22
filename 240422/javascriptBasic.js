cdd2c7d259a0beb8bdf3637a0038648dcc38b78e
MOONWUD <moonqazwsx@naver.com>
2024-04-22 12:44:56 +0900

    240422 중간업로드

---
+++ 240422/javascriptBasic.js
@@ -0,0 +1,18 @@
+// Object.method(parameter); 객체.메서드(매개변수); 
+// "."가 연결되었다는 의미고 ";"가 끝났다는 말
+
+console.log("메시지 출력"); //콘솔 영역에 출력 (브라우저에서 바로 실행되는 것)
+
+/*
+  객체는 도서관같은 것. 정리가 잘 되어있는 엄청나게 큰 ,,,
+  .log라는 열람실을 가져온 것
+*/
+
+console.debug("log와 마찬가지로 콘솔 영역에 출력");
+console.clear();
+
+
+// 문서에 hello JavaScript 작성
+document.querySelector("body").innerHTML = "Hello JavaScript";
+
+// docment객체에서.... innerHTMl은 html안에 java를 넣겠다 라는 의미 (근데 body가 없어 그래서 실행 안돼)
