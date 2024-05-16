9f6be4a499b8d9ef68a0497eeb72c4c32a16a4e0
MOONWUD <moonqazwsx@naver.com>
2024-05-16 12:16:57 +0900

    240507 마무리

---
+++ 240516_jQurey/swiper.js
@@ -0,0 +1,12 @@
+const mainSwiper = new Swiper(".main-swiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
